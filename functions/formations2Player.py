ba80d8de2b28077cf7e8453cf87512d4f44b1204
Nick Kapur <npkapur@ncsu.edu>
2018-04-12 12:12:45 -0400

    Formations and img file progress

--- functions/formations2Player.py
+++ functions/formations2Player.py
@@ -1,9 +1,15 @@
 import mss
 import sys
 import img
+import time
 
-def play(home_pi, away_pi, controller, monitor):
-    # Load screen type model that give "other", "playcall", "kickoff", or "endgame"
+# TODO   line 12  : actual model needs to be loaded
+# TODO end of file: action sequence needs to be written
+
+
+def play(home_pi, away_pi, controller, monitor, difficulty):
+    # Load screen type model that give "other","playcall","kickoff",or"endgame"
+    screen_type_model = None
 
     with mss.mss() as sct:
         if monitor == "1":
@@ -19,4 +25,29 @@ def play(home_pi, away_pi, controller, monitor):
         screen_type = "other"
 
         while screen_type == "other":
-            frame = img.prepFrame(sct, monitor)
+            frame = img.prepFrame(sct, monitor, gray = True)
+
+            if frame == "Bad Frame":
+                continue
+
+            screen_check_1 = img.getPrediction(frame, screen_type_model)
+
+            if screen_check_1 == "end_game":
+                time.sleep(1)
+                return None
+
+            if screen_check_1 != "other":
+                frame_2 = img.prepFrame(sct, monitor, gray = True)
+
+                screen_check_2 = img.getPrediction(frame_2, screen_type_model)
+
+                if screen_check_1 == screen_check_2:
+                    break
+                else:
+                    screen_type = "other"
+
+        if screen_check_2 == "kickoff":
+            pi.send("Press A")
+            time.sleep(7)
+            pi.kick("center", difficulty)
+            continue
--- functions/img.py
+++ functions/img.py
@@ -1,2 +1,15 @@
+import numpy as np
+import cv2
+
+
 def prepFrame(sct, monitor, gray = False):
-    pass
+    frame = np.array(sct.grab(monitor))
+    frame = cv2.resize(frame, (1200, 800))
+    if gray:
+        try:
+            gray = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
+            return gray
+        except Exception:
+            return "Bad Frame"
+    else:
+        return frame
